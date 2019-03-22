4bbcb79aefacb41ec8bc9b87e316e1050b073625
Rosa <arduinoproyect@gmail.com>
2019-03-21 21:30:21 -0600

    Se sube versión desde casa

--- server/config/config.js
+++ server/config/config.js
@@ -34,3 +34,8 @@ process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
 //===================
 
 process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo'
+
+//===================
+//Google Client ID
+//===================
+process.env.CLIENT_ID = process.env.CLIENT_ID || '669183183240-mra1qvgph2n9r4cfl56tb8b37l09f2ea.apps.googleusercontent.com'
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config')
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 
 const app = express();
@@ -12,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+//Habilitar la carpeta public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // app.use(function(req, res) {
 //     res.setHeader('Content-Type', 'text/plain')

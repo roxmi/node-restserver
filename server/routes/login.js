036a28f6af025cbbed298324effa9125b630f915
Rosa <arduinoproyect@gmail.com>
2019-03-21 18:08:56 -0600

    Google Sing-In

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { OAuth2Client } = require('google-auth-library');
+const client = new OAuth2Client(process.env.CLIENT_ID);
+
 const Usuario = require('../models/usuario');
 require('../config/config')
 const app = express();
 
-app.post('/login', function (req, res) {
+app.post('/login', function(req, res) {
     let body = req.body;
     Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
         if (err) {
@@ -30,13 +33,11 @@ app.post('/login', function (req, res) {
                 }
             });
         }
-        let token = jwt.sign(
-            {
-                usuario: usuarioDB
-            }, process.env.SEED,
-            {
-                expiresIn: process.env.CADUCIDAD_TOKEN
-            });
+        let token = jwt.sign({
+            usuario: usuarioDB
+        }, process.env.SEED, {
+            expiresIn: process.env.CADUCIDAD_TOKEN
+        });
         return res.json({
             ok: true,
             usuario: usuarioDB,
@@ -46,5 +47,86 @@ app.post('/login', function (req, res) {
     })
 });
 
+//Configuraciones de Google
+async function verify(token) {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+        // Or, if multiple clients access the backend:
+        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+    });
+    const payload = ticket.getPayload();
+
+    return {
+        nombre: payload.name,
+        email: payload.email,
+        img: payload.picture,
+        google: true
+    }
+    //const userid = payload['sub'];
+    // If request specified a G Suite domain:
+    //const domain = payload['hd'];
+}
+
+app.post('/google', async(req, res) => {
+    let token = req.body.idtoken;
+    let googleUser = await verify(token).catch(e => {
+        return res.status(403).json({
+            ok: false,
+            err
+        });
+    });
+    Usuario.findOne({ email: googleUser.email }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+        if (usuarioDB) {
+            if (usuarioDB.google === false) {
+                return res.status(400).json({
+                    ok: false,
+                    err: {
+                        message: 'Debe de usar su autenticación normal'
+                    }
+                });
+            } else {
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, {
+                    expiresIn: process.env.CADUCIDAD_TOKEN
+                });
+                res.json({ ok: true, usuario: usuarioDB, token });
+            }
+        } else {
+            //si no existe en BD
+            let usuario = new Usuario();
+            usuario.nombre = googleUser.nombre;
+            usuario.email = googleUser.email;
+            usuario.img = googleUser.img;
+            usuario.google = true;
+            usuario.password = ':)';
+
+            usuario.save((err, usuarioDB) => {
+                if (err) {
+                    return res.status(500).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                let token = jwt.sign({
+                    usuario: usuarioDB
+                }, process.env.SEED, {
+                    expiresIn: process.env.CADUCIDAD_TOKEN
+                });
+                res.json({ ok: true, usuario: usuarioDB, token });
+            });
+
+        }
+    });
+
+});
 
 module.exports = app;
